358e5167c932a903e4c41eb440228053154b39ea
Kai Patrick <thiskaiguy@gmail.com>
2018-09-08 17:04:41 +0100

    Export all type aliases from inside the KoaMatchPath namespace

--- index.d.ts
+++ index.d.ts
@@ -3,50 +3,52 @@ import pathToRegexp = require('path-to-regexp')
 
 export = KoaPathMatch
 
-declare function KoaPathMatch(options?: pathToRegexp.RegExpOptions): RouterFactory
+declare function KoaPathMatch(options?: pathToRegexp.RegExpOptions): KoaPathMatch.RouterFactory
 
-type RouterFactory = {
-  (path: pathToRegexp.Path, fn: Koa.Middleware): Router
-  (path: pathToRegexp.Path, fn: Koa.Middleware[]): Router
-  (path: pathToRegexp.Path, ...fn: Koa.Middleware[]): Router
+declare namespace KoaPathMatch {
+  export type RouterFactory = {
+    (path: pathToRegexp.Path, fn: Koa.Middleware): Router
+    (path: pathToRegexp.Path, fn: Koa.Middleware[]): Router
+    (path: pathToRegexp.Path, ...fn: Koa.Middleware[]): Router
+  }
+
+  export type Router = Koa.Middleware & {
+    [M in HTTPMethod]: (ctx: Koa.Middleware) => Router
+  }
+
+  export type HTTPMethod
+    = 'acl'
+    | 'bind'
+    | 'checkout'
+    | 'connect'
+    | 'copy'
+    | 'delete'
+    | 'get'
+    | 'head'
+    | 'link'
+    | 'lock'
+    | 'm-search'
+    | 'merge'
+    | 'mkactivity'
+    | 'mkcalendar'
+    | 'mkcol'
+    | 'move'
+    | 'notify'
+    | 'options'
+    | 'patch'
+    | 'post'
+    | 'propfind'
+    | 'proppatch'
+    | 'purge'
+    | 'put'
+    | 'rebind'
+    | 'report'
+    | 'search'
+    | 'source'
+    | 'subscribe'
+    | 'trace'
+    | 'unbind'
+    | 'unlink'
+    | 'unlock'
+    | 'unsubscribe'
 }
-
-type Router = Koa.Middleware & {
-  [M in HTTPMethod]: (ctx: Koa.Middleware) => Router
-}
-
-type HTTPMethod
-  = 'acl'
-  | 'bind'
-  | 'checkout'
-  | 'connect'
-  | 'copy'
-  | 'delete'
-  | 'get'
-  | 'head'
-  | 'link'
-  | 'lock'
-  | 'm-search'
-  | 'merge'
-  | 'mkactivity'
-  | 'mkcalendar'
-  | 'mkcol'
-  | 'move'
-  | 'notify'
-  | 'options'
-  | 'patch'
-  | 'post'
-  | 'propfind'
-  | 'proppatch'
-  | 'purge'
-  | 'put'
-  | 'rebind'
-  | 'report'
-  | 'search'
-  | 'source'
-  | 'subscribe'
-  | 'trace'
-  | 'unbind'
-  | 'unlink'
-  | 'unlock'
-  | 'unsubscribe'
